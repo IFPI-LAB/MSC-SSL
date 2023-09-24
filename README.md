e1b71912673014d710d8e88621d7a35f0810d9f2
shark <519029617@qq.com>
2023-09-24 23:27:32 +0800

    commit

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # MSC-SSL
-The code is being organized ... ...
+This repository is the official code for the paper "Cost-effective identification of the field maturity of tobacco leaves based on deep semi-supervised learning and smartphone photograph".
+
+## Installation
+Clone this repo.
+```bash
+git clone https://github.com/IFPI-LAB/MSC-SSL.git
+cd MSC-SSL/
+```
+
+We have tested our code in the following environment:
+- Python 3.8
+- Pytorch 1.9.0
+- Torchvision 0.10.0
+- CUDA 11.1
+
+## Data Preparation
+The dataset is not publicly available, you may contact the corresponding author for research purposes to obtain the dataset.
+
+You can organize your dataset into directories, as shown below:
+```
+- data_root
+    - labeled
+        - labeled_folder
+            - class_1
+            - class_2
+            - ...
+    - train
+        - class_1
+        - class_2
+        - ...
+    - test
+```
+
+## Running the model
+
+### Baseline methods
+To run the main Python file, use the following command:
+
+```sh
+python main.py --data_root your/data/path --model_save_path your/save/path --labeled_folder your/labeled/data
+```
+This command runs the MSC-SSL model with the specified hyperparameters. You can modify the command to run different experiments with different hyperparameters.
+
+## Acknowledgement
+This code is partially based on [DeepDA](https://github.com/jindongwang/transferlearning/tree/master/code/DeepDA), [Semi-Supervised-Transfer-Learning](https://github.com/SHI-Labs/Semi-Supervised-Transfer-Learning).
--- parser1.py
+++ parser1.py
@@ -7,17 +7,18 @@ def default_parser():
     parser.add_argument("--data_root", "-r", default=r"..\data\1-bottom", type=str, help="/path/to/dataset")
     parser.add_argument('--labeled_folder', type=str, default=None, help="folder name of labeled data")
     parser.add_argument('--model_save_path', type=str, default='output/1-bottom_msc_ssl')
-    parser.add_argument("--num_labels", default=30, type=int, help="number of labeled data")  # if labeled folder is not None, this parameter will not work
-    parser.add_argument('--lambda_lmmd', type=float, default=1, help='data_mmd_loss_ratio')
-    parser.add_argument('--lambda_level_l', type=float, default=0.5, help='data_level_loss_ratio')
-    parser.add_argument('--lambda_level_u', type=float, default=0.1, help='data_level_loss_ratio')
+    parser.add_argument("--num_labels", default=-1, type=int, help="number of labeled data")  # if labeled folder is not None, this parameter will not work
+    parser.add_argument('--lambda_lmmd', type=float, default=1, help='weight parameters of lmmd loss')
+    parser.add_argument('--lambda_level_l', type=float, default=0.5, help='weight parameters of maturity structure constraint loss: labeled term')
+    parser.add_argument('--lambda_level_u', type=float, default=0.1, help='weight parameters of maturity structure constraint loss: unlabeled term')
 
     # dataset config
     parser.add_argument("--dataset", "-d", default="tobacco",  type=str, help="dataset name")
-    parser.add_argument("--num_unlabels", default=-1, type=int, help="number of unlabeled data")  # <0, unlabeled data为所有训练集，包括了labeled data；>0，随机选取num_unlabels个未标记样本+已选择的有标记样本
+    parser.add_argument("--num_unlabels", default=-1, type=int, help="number of unlabeled data")  # If num_unlabels<0, use all training datasets as unlabeled samples
     parser.add_argument("--num_workers", default=1, type=int, help="number of thread for CPU parallel")
+
     # optimization config
-    parser.add_argument("--model", default="resnet", type=str, help="model architecture") #resner or wideresnetleaky
+    parser.add_argument("--model", default="resnet", type=str, help="model architecture")
     parser.add_argument("--depth", default=50,  type=int, help="model depth")
     parser.add_argument("--widen_factor", default=1,  type=int, help="widen factor for wide resnet")
     parser.add_argument("--bn_momentum", default=0.001,  type=float, help="bn momentum for wide resnet")
@@ -26,24 +27,28 @@ def default_parser():
     parser.add_argument("--lr", default=0.01, type=float, help="learning rate")
     parser.add_argument("--weight_decay", "-wd", default=0.0001, type=float, help="weight decay")
     parser.add_argument("--momentum", default=0.9, type=float, help="momentum for sgd or beta_1 for adam")
-    parser.add_argument('--per_epoch_steps', type=int, default=100, help='number of training images for each epoch') #  1000
-    parser.add_argument("--epochs", default=100, type=int, help="number of training epochs") # iterations 1000000
+    parser.add_argument('--per_epoch_steps', type=int, default=100, help='number of training images for each epoch')
+    parser.add_argument("--epochs", default=100, type=int, help="number of training epochs")
     parser.add_argument("--num_cycles", default=7.9/16, type=float, help="num cycle for CosineAnnealingLR")
-    parser.add_argument("--merge_one_batch", default=0 ,type=int, help="interleave of not")
-    parser.add_argument("--interleave", default=1 ,type=int, help="interleave of not")
-    ## LMMD loss
-    parser.add_argument('--reg_warmup', type=int, default=10, )
-    parser.add_argument('--reg_warmup_iter', type=int, default=100, ) # 100
-    parser.add_argument('--lmmd_feat_table_l', type=int, default=128, help='feat size for mmd table') # 128
-    parser.add_argument('--lmmd_feat_table_u', type=int, default=128, help='feat size for mmd table') # 128
+    parser.add_argument("--merge_one_batch", default=0, type=int, help="interleave of not")
+    parser.add_argument("--interleave", default=1, type=int, help="interleave of not")
+
+    # LMMD loss
+    parser.add_argument('--reg_warmup', type=int, default=10)
+    parser.add_argument('--reg_warmup_iter', type=int, default=100)
+    parser.add_argument('--lmmd_feat_table_l', type=int, default=128, help='feat size for mmd table')
+    parser.add_argument('--lmmd_feat_table_u', type=int, default=128, help='feat size for mmd table')
     parser.add_argument('--lmmd_threshold', default=0.7, type=float, help='lmmd loss threshold in terms of outputs entropy')
-    ## transfer learning
+
+    # transfer learning
     parser.add_argument('--pretrained', type=int, default=1)
-    parser.add_argument('--imprint', type=int, default=1,help='imprint for pretrained classifier')
+    parser.add_argument('--imprint', type=int, default=1, help='imprint for pretrained classifier')
+
     # evaluation checkpoint config
     parser.add_argument("--eval_every", default=1, type=int, help="eval every N epoches")
     parser.add_argument("--save_every", default=200, type=int, help="save every N epoches")
     parser.add_argument("--resume", default=None, type=str, help="path to checkpoint model")
+
     # misc
     parser.add_argument("--seed", default=1, type=int, help="random seed")
     parser.add_argument("--pretrained_weight_path", default="ckpt", type=str, help="pretrained_weight_path")

e186e618b3fc97a1df07f791df59607a9fb61e84
DESKTOP-9A6DDCG\zheng <519029617@qq.com>
2023-12-05 16:33:57 +0800

    train_simsiam update

--- simsiam/train_simsiam.py
+++ simsiam/train_simsiam.py
@@ -32,7 +32,7 @@ model_names = sorted(name for name in models.__dict__
     and callable(models.__dict__[name]))
 
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
-parser.add_argument('--data', metavar='DIR',
+parser.add_argument('--data_root', metavar='DIR',
                     help='path to dataset')
 parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet50',
                     choices=model_names,
@@ -271,8 +271,8 @@ def main_worker(gpu, ngpus_per_node, args):
     cudnn.benchmark = True
 
     # Data loading code
-    traindir = os.path.join(args.data, 'train')
-    valdir = os.path.join(args.data, 'val')
+    traindir = os.path.join(args.data_root, 'train')
+    valdir = os.path.join(args.data_root, 'val')
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
 
